292cd33c0c8662957c4d97a81faa430f0a90c233
Dhyanesh2098 <dhyaneshreddydamireddy@gmail.com>
2026-05-25 06:36:46 +0530

    Use deployed backend API URLs

--- client/src/services/activityService.js
+++ client/src/services/activityService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/activities/";
+const API_URL = "https://task-manager-backend-0e4l.onrender.com/api/activities/";
 
 const getActivities = async () => {
   const user = JSON.parse(localStorage.getItem("user"));
